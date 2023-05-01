16319c7a7f3f068ae750f740f02706ee21780842
Binary Craft <keep.it.sns@gmail.com>
2023-05-02 01:17:32 +0800

    add objcopy step

--- build.zig
+++ build.zig
@@ -7,17 +7,21 @@ const microzig = @import("deps/stmicro-stm32/deps/microzig/build.zig");
 pub fn build(b: *std.build.Builder) !void {
     const optimize = b.standardOptimizeOption(.{});
     var exe = microzig.addEmbeddedExecutable(b, .{
-        .name = "hello",
+        .name = "hello.elf",
         .source_file = .{
             .path = "src/main.zig",
         },
         .backing = .{
             .chip = stm32.chips.stm32f103x8,
-
-            // instead of a board, you can use the raw chip as well
-            // .chip = atmega.chips.atmega328p,
         },
         .optimize = optimize,
     });
     exe.installArtifact(b);
+
+    const bin = exe.inner.addObjCopy(.{
+        .format = .bin,
+        .basename = "hello.bin",
+    });
+    const bin_install = b.addInstallBinFile(bin.getOutputSource(), bin.basename);
+    b.getInstallStep().dependOn(&bin_install.step);
 }
